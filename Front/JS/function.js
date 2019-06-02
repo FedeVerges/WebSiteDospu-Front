8612f64d02181cf15dedbf37611ec03b0a8beee6
eugeformoso <eugeformoso247@gmail.com>
2019-06-01 22:14:34 -0300

    orden web

--- Front/JS/function.js
+++ Front/JS/function.js
@@ -29,7 +29,35 @@ function createListNavBar(lista) {
 
 }
 
+/* para cambiar de pasos*/
+let form= document.querySelector('.form-orden'); 
+let progressOption= document.querySelectorAll('.progressbar_option')
 
+form.addEventListener('click',function(e){
+    let element= e.target;
+    let isButtonNext= element.contList.contains('step_button-next');
+    let isButtonBack= element.contList.contains('step_button-back');
+    if(isButtonNext || isButtonBack){
+        let currentStep= document.getElementById('step-'+ element.dataset.step);
+        let jumptStep= document.getElementById('step-'+ element.dataset.to_step);
+        currentStep.addEventListener('animationend',function callback(){
+            currentStep.classList.remove('active');
+            jumptStep.classList.add('active');
+            if(isButtonNext){
+                currentStep.classList.add('to-left');
+                progressOption[element.dataset.to_step -1].classList.add('active');
+            }
+            else{
+                jumptStep.classList.remove('to-left');
+                progressOption[element.dataset.step -1].classList.remove('active');
+            }
+            currentStep.removeEventListener('animationend',callback);
+        })
+        currentStep.classList.add('inactive');
+        jumptStep.classList.remove('inactive');
+    }
+
+})
 
 
 
