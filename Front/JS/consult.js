1241265e3e544c6a29cf784421cacb0330e03e95
fefo <fede_16_98@hotmail.com>
2019-06-10 01:09:51 -0300

    Fix validation error

--- Front/JS/consult.js
+++ Front/JS/consult.js
@@ -15,31 +15,39 @@ function sendSuggestion() {
     const sendButton = document.getElementById('suggestion-send-button')
 
     sendButton.onclick = () => {
-        const name = document.getElementById('suggestion-name').value
-        const surname = document.getElementById('suggestion-surname').value
-        const email = document.getElementById('suggestion-email').value
-        const consult = document.getElementById('suggestion-suggestion').value
 
-        const suggestion1 = new Suggestion(name, surname, email, consult,"2/05/2019","123123asdasd")
+        const name = document.getElementById('suggestion-name')
+        const surname = document.getElementById('suggestion-surname')
+        const email = document.getElementById('suggestion-email')
+        const consult = document.getElementById('suggestion-suggestion')
 
-        fetch("http://localhost:9000/api/suggestions", {
-            method: 'post',
-            mode: "cors",
-            headers: new Headers({
-                'content-type': 'application/json'
-            }),
-            body: JSON.stringify({
-                Name: suggestion1.name,
-                Surname: suggestion1.surname,
-                Email: suggestion1.email,
-                Message: suggestion1.message,
-                Date: suggestion1.date,
-                Id: suggestion1.id,
+        if (name.checkValidity() & surname.checkValidity() & email.checkValidity() & consult.checkValidity()) {
 
-            })
 
-        }).then(handleSuggestionResponse)
+            const suggestion1 = new Suggestion(name, surname, email, consult, "2/05/2019", "123123asdasd")
+
+            fetch("http://localhost:9000/api/suggestions", {
+                method: 'post',
+                mode: "cors",
+                headers: new Headers({
+                    'content-type': 'application/json'
+                }),
+                body: JSON.stringify({
+                    Name: suggestion1.name,
+                    Surname: suggestion1.surname,
+                    Email: suggestion1.email,
+                    Message: suggestion1.message,
+                    Date: suggestion1.date,
+                    Id: suggestion1.id,
+
+                })
+
+            }).then(handleSuggestionResponse)
+        } else {
+            alert('Validation Error')
+        }
     }
+
 
 
 }
