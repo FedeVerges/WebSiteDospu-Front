0cd65472c38b7253da1fc765383a4167fc004215
fefo <fede_16_98@hotmail.com>
2019-06-14 20:13:28 -0300

    take information from the Backend's Api

--- Front/JS/form.js
+++ Front/JS/form.js
@@ -36,37 +36,54 @@ class OrderType {
             this.orderCost = orderCost;
     }
 }
+
+/* get Lenders */
+
+function getLenders() {
+    const siguiente = document.getElementById("button-Next1")
+
+    const lenderArray = []
+    siguiente.addEventListener('click', async () => {
+
+        const response = await fetch('http://localhost:9000/api/lenders/Consulta%20m%C3%A9dica/');
+        const myJson = await response.json(); //extract JSON from the http response
+        putLenders(myJson)
+    })
+}
+
+/* End get Lenders */
+
+
 /* Partners */
 const p1 = new Partner("Federico", "Verges", "41221778", "5/5/2019", "Titular", "12312312");
 const p2 = new Partner("Alfredo", "Verges", "41221777", "10/5/2019", "Titular", "22312312");
 
 
-/* Lenders  */
-const l1 = new Lender("Facundo", "Decena","20121231238")
-const l2 = new Lender("Facundo", "Pereira","20121231233")
-
 /* OrderType */
 
-const ot1 = new OrderType ("50c","tipo1",50)
-const ot2 = new OrderType ("50d","tipo2",150)
+const ot1 = new OrderType("50c", "Consulta Médica", 50)
+const ot2 = new OrderType("50d", "Consulta Psicológica", 100)
+const ot3 = new OrderType("50e", "Sesion de Psicoterapia Individual", 150)
+const ot4 = new OrderType("50f", "Practica Odontológica", 200)
+
 
 const arregloOrdenes = []
-const arregloPrestadores= []
-const arregloAfiliados= []
+const arregloPrestadores = []
+const arregloAfiliados = []
 
 arregloAfiliados.push(p1);
 arregloAfiliados.push(p2);
 
 arregloOrdenes.push(ot1);
 arregloOrdenes.push(ot2);
-
-arregloPrestadores.push(l1);
-arregloPrestadores.push(l2);
+arregloOrdenes.push(ot3);
+arregloOrdenes.push(ot4);
 
 TakeOrder(arregloOrdenes);
 showPerson(arregloAfiliados);
-putLenders(arregloPrestadores);
 showOrderCost(arregloOrdenes);
+getLenders();
+
 showInformation();
 pushOrder();
 
@@ -86,12 +103,11 @@ function TakeOrder(arregloOrdenes) {
 
 function putLenders(arregloPrestadores) {
     const selector = document.getElementById("prestador");
-    console.log(selector);
 
     for (let index = 0; index < arregloPrestadores.length; index++) {
         const orderElement = document.createElement("option");
-        orderElement.setAttribute("value",arregloPrestadores[index].name);
-        const texto = document.createTextNode(arregloPrestadores[index].name);
+        orderElement.setAttribute("value", arregloPrestadores[index].Name);
+        const texto = document.createTextNode(arregloPrestadores[index].Name + ' ' +arregloPrestadores[index].Surname );
         orderElement.appendChild(texto);
         selector.appendChild(orderElement);
     }
@@ -128,24 +144,10 @@ function pushOrder() {
                     'content-type': 'application/json'
                 }),
                 body: JSON.stringify({
-
-                    OrderNumber: webOrder.orderNumber,
-                    Date: webOrder.date,
-                    AuthCode: webOrder.authCode,
-                    BarsCode: webOrder.BarsCode,
-                    Lender: {
-                        Name: lender.name,
-                        Surname: lender.surname,
-                        Cuil: lender.cuil
-                    },
-                    Partner: {
-                        Name: afiliado.name,
-                        Surname: afiliado.surname,
-                        Dni: afiliado.dni,
-                        Birthdate: afiliado.birthdate,
-                        Type: afiliado.type,
-                        Id: afiliado.id
-                    },
+                    PartnerDni: afiliado.dni,
+                    PartnerName: afiliado.name,
+                    PartnerSurname: afiliado.surname,
+                    LenderCuil: lender.cuil,
                     OrderType: {
                         orderTypeId: tipoOrden.orderTypeID,
                         Type: tipoOrden.type,
@@ -158,9 +160,6 @@ function pushOrder() {
             .then(handleWebOrderResponse)
     }
 }
-
-
-
 /* This method send the dni number to the DOSPU's API and then 
 it give me a confirmation and the name and surname of the  beneficiaries */
 function getBeneficiary() {
@@ -203,55 +202,53 @@ function handleWebOrderResponse(response) {
 
 /*Formulario */
 
-function showOrderCost(arrayOrder){
-    const select= document.getElementById("tipo-orden");
+function showOrderCost(arrayOrder) {
+    const select = document.getElementById("tipo-orden");
     select.addEventListener('change',
-    function(){
-        const selectedOption = this.options[select.selectedIndex];
-        for (let i = 0; i < arrayOrder.length; i++) {
-            if (arrayOrder[i].type == selectedOption.text) {
-                document.getElementById("valor").value= arrayOrder[i].orderCost;
+        function () {
+            const selectedOption = this.options[select.selectedIndex];
+            for (let i = 0; i < arrayOrder.length; i++) {
+                if (arrayOrder[i].type == selectedOption.text) {
+                    document.getElementById("valor").value = arrayOrder[i].orderCost;
+                }
             }
-        } 
-    });
+        });
 }
 
-function showPerson(array){
+function showPerson(array) {
     const buttonSearch = document.getElementById("button-search");
-    buttonSearch.addEventListener('click', function(e){
-        const result= givePerson(array);
+    buttonSearch.addEventListener('click', function (e) {
+        const result = givePerson(array);
         if (result != null) {
-            document.getElementById("nombre-afiliado").value=result.name;
-            document.getElementById("apellido-afiliado").value=result.surname;
-            document.getElementById("button-Next1").disabled=false;
-        }
-        else{
+            document.getElementById("nombre-afiliado").value = result.name;
+            document.getElementById("apellido-afiliado").value = result.surname;
+            document.getElementById("button-Next1").disabled = false;
+        } else {
             alert("El DNI ingresado no se cuentra en la Base de Datos");
         }
     });
 }
 
-function showInformation(){
+function showInformation() {
     const buttonNext = document.getElementById("button-Next2");
-    buttonNext.addEventListener("click",function(e){
+    buttonNext.addEventListener("click", function (e) {
         document.getElementById("nombre-afiliado-show").value = document.getElementById("nombre-afiliado").value;
         document.getElementById("apellido-afiliado-show").value = document.getElementById("apellido-afiliado").value;
         document.getElementById("DNI-afiliado-show").value = document.getElementById("i-dni").value;
-        
+
         document.getElementById("tipo-orden-show").value = document.getElementById("tipo-orden").value;
         document.getElementById("prestador-show").value = document.getElementById("prestador").value;
-        
-        document.getElementById("valor-show").value= document.getElementById("valor").value;
+
+        document.getElementById("valor-show").value = document.getElementById("valor").value;
     });
 }
 
-function givePerson(arrayPartner){
+function givePerson(arrayPartner) {
     const DNI = document.getElementById("i-dni").value;
     for (let i = 0; i < arrayPartner.length; i++) {
         if (arrayPartner[i].dni == DNI) {
             return arrayPartner[i];
-        }
-        else{
+        } else {
             return null;
         }
     }
